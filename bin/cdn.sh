36666b3fd79cd10513d7473f5187197eb9d9de8e
Robin Houston <robin.houston@gmail.com>
2017-01-19 20:30:18 +0000

    Fix CDN upload paths

--- bin/cdn.sh
+++ bin/cdn.sh
@@ -11,5 +11,6 @@ version=$1
 npm run prepublish
 aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider.js s3://cdn.flourish.rocks/slider-v"$version".js
 aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider-full.js s3://cdn.flourish.rocks/slider-v"$version".full.js
-aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider.js s3://cdn.flourish.rocks/slider-v"$version".js
-aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider-full.js s3://cdn.flourish.rocks/slider-v"$version".full.min.js
+
+aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider.min.js s3://cdn.flourish.rocks/slider-v"$version".min.js
+aws --profile=kiln s3 --region=eu-west-1 cp --acl=public-read slider-full.min.js s3://cdn.flourish.rocks/slider-v"$version".full.min.js
